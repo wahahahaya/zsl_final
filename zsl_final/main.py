733fa3696d3bf31c155e5ee9b7c1bbf83ba5652d
Arlen Chen <a870128rlen@gmail.com>
2022-07-15 12:50:55 +0800

    rename

--- zsl_final/main.py
+++ zsl_final/main.py
@@ -12,7 +12,7 @@ from apex import amp
 import parameter
 import build_data
 import build_model
-from build_loss import cpt_loss, ad_loss
+from build_loss import cpt_loss
 from resnet_feature import resnet101_features
 from classifier import eval_gzsl
 from utils import write_json, make_optimizer, make_lr_scheduler
@@ -36,7 +36,9 @@ def synchronize():
 def main(config):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    seed = 214
+    # seed = random.randint(1, 10000)
+    seed = 7384
+    print("Random Seed: ", seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
@@ -60,64 +62,60 @@ def main(config):
     amp_opt_level = 'O1' if use_mixed_precision else 'O0'
     model, optimizer = amp.initialize(model, optimizer, opt_level=amp_opt_level)
 
-    loss_cls = nn.CrossEntropyLoss().to(device)
-    loss_ad = ad_loss(config.dataset_name).to(device)
-    loss_cpt = cpt_loss(device).to(device)
-    loss_reg = nn.MSELoss().to(device)
+    loss_cls = nn.CrossEntropyLoss()
+    loss_cpt = cpt_loss(device)
+    loss_reg = nn.MSELoss()
 
     losses = []
     cls_losses = []
     reg_losses = []
-    ad_losses = []
     cpt_losses = []
     best_H = 0
+
     for epoch in range(0, config.epochs):
         loss_epoch = []
         cls_loss_epoch = []
         reg_loss_epoch = []
-        ad_loss_epoch = []
         cpt_loss_epoch = []
 
-        scheduler.step()
         for i, (img, att, label) in enumerate(train_dataloader):
             batch_img = img.to(device)
             batch_att = att.to(device)
             batch_label = label.to(device)
 
-            score, part_feat, atten_map, atten_attr, query = model(batch_img, seen_att=att_seen, mode="train")
+            score, global_feat, part_feat, atten_map, atten_attr, query = model(batch_img, seen_att=att_seen, mode="train")
 
             Lcls = loss_cls(score, batch_label)
             Lreg = loss_reg(atten_attr, batch_att)
-            Lad = loss_ad(query)
             Lcpt = loss_cpt(atten_map)
 
-            loss = Lcls + Lreg + Lad + Lcpt
+            # loss = Lcls + config.lamd1*Lreg + config.lamd3*Lcpt
+            loss = Lcls
 
             optimizer.zero_grad()
             with amp.scale_loss(loss, optimizer) as scaled_losses:
                 scaled_losses.backward()
+            # loss.backward()
             optimizer.step()
 
             loss_epoch.append(loss.item())
             cls_loss_epoch.append(Lcls.item())
             reg_loss_epoch.append(Lreg.item())
-            ad_loss_epoch.append(Lad.item())
             cpt_loss_epoch.append(Lcpt.item())
 
+        scheduler.step()
         losses += loss_epoch
         cls_losses += cls_loss_epoch
         reg_losses += reg_loss_epoch
-        ad_losses += ad_loss_epoch
         cpt_losses += cpt_loss_epoch
 
         losses_mean = sum(losses) / len(losses)
         cls_losses_mean = sum(cls_losses) / len(cls_losses)
         reg_losses_mean = sum(reg_losses) / len(reg_losses)
-        ad_losses_mean = sum(ad_losses) / len(ad_losses)
         cpt_losses_mean = sum(cpt_losses) / len(cpt_losses)
 
-        print("Epoch: %d/%d, cls loss:%.4f, reg loss:%.4f, ad loss:%.4f, cpt loss:%.4f, loss:%.4f" % (
-                epoch+1, config.epochs, cls_losses_mean, reg_losses_mean, ad_losses_mean, cpt_losses_mean, losses_mean
+        print("Epoch: %d/%d, cls loss:%.4f, reg loss:%.4f, cpt loss:%.4f, loss:%.4f" % (
+                epoch+1, config.epochs, cls_losses_mean, reg_losses_mean, cpt_losses_mean, losses_mean
             )
         )
 
@@ -141,17 +139,22 @@ def main(config):
             best_acc_seen = acc_seen
             best_acc_unseen = acc_unseen
             best_H = H
+            # torch.save(model.state_dict(), "log/model/AWA2_%d_%d_%d.pth" % (best_H*1000, best_epoch, seed))
+            # print("save model")
 
     print("best: ep: %d" % best_epoch)
     print('train: %.4f, gzsl: seen=%.4f, unseen=%.4f, h=%.4f' % (best_acc_train, best_acc_seen, best_acc_unseen, best_H))
+    print("################################")
 
 
 if __name__ == "__main__":
+    torch.backends.cudnn.deterministic = True
     config = parameter.get_parameters()
-    log = config.__dict__
-    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log.update({'time': time})
-    if not os.path.isdir(config.tensorboard_dir):
-        os.mkdir(config.tensorboard_dir)
-    write_json(log, os.path.join(config.tensorboard_dir+"/config.json"))
+    # log = config.__dict__
+    # time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # log.update({'time': time})
+    # if not os.path.isdir(config.tensorboard_dir):
+    #     os.mkdir(config.tensorboard_dir)
+    # write_json(log, os.path.join(config.tensorboard_dir+"/config_7384_AWA2.json"))
+    # for _ in range(30):
     main(config)
